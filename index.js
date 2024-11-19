cdd4dd6d0ffec9b8c265dd4d2365f2922e9ee6b0
Hema Bindra <hbindra11@yahoo.com>
2024-11-19 16:10:27 +0100

    set up connection to neon postgreSQL database

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 import pg from "pg";
+import express from "express";
+import cors from 'cors';
 const { Client } = pg;
-const client = new Client();
-await client.connect();
+const app = express();
+const port = 3000;
 
+// connecting to my postgreSQL db --- was successful!!
+const client = new Client({
+  connectionString: process.env.PG_URI,
+});
+await client.connect();
+//app.use(express.json());
+app.use(cors());
+let result = ''
 try {
-  const reponse = await client.query("SELECT $1::text as message", [
-    "Hello world",
-  ]);
-  console.log(reponse.rows[0].message);
+  //   const reponse = await client.query("SELECT $1::text as message", [
+  //     "Hello world!!",
+  //   ]);
+  const response = await client.query(
+    "SELECT * FROM blogPosts WHERE id = $1;",
+    [1]
+  );
+  console.log(response.rows[0]);
+  result = response.rows[0];
 } catch (error) {
   console.error(error);
 } finally {
   await client.end();
 }
+console.log(result);
+
+
+// using express to handel http requests and responses -- and server runs alright!!
+//app.get("/", (request, response) => {response.send("Hello, World again!!!")});
+app.get('/', (request, response )=> {response.send(result)});
+app.listen(port, () => console.log(`Server is running on port ${port} `));
