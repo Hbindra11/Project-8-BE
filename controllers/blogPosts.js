640ec6193f6ab73a7776c98dfe163f0d8b0ee957
Hema Bindra <hbindra11@yahoo.com>
2024-11-20 16:12:08 +0100

    all crud operations work against neon postgressql table: BlogPosts

--- controllers/blogPosts.js
+++ controllers/blogPosts.js
@@ -5,15 +5,70 @@ export const createBlogPost = async (req, res) => {
     const {
       body: { author, title, content, cover },
     } = req;
-   // console.log('my blog: '+req.json);
+    // console.log('my blog: '+req.json);
+    if (!author || !title || !content || !cover)
+      return res
+        .status(400)
+        .json({ error: "Fields: author,title,content, and cover are all required!" });
+
+    const blogPost = await BlogPost.create(req.body);
+    res.json(blogPost);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getBlogs = async (req, res) => {
+  try {
+    const blogs = await BlogPost.findAll();
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const blog = await BlogPost.findByPk(id);
+    if (!blog) return res.status(400).json({ error: "Blog not found!" });
+    res.json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateBlog = async (req, res) => {
+  try {
+    const {
+      body: { author, title, content, cover },
+      params: { id },
+    } = req;
     if (!author || !title || !content || !cover)
       return res
         .status(400)
         .json({ error: "author,title,content, and cover are all required!" });
-        
-     const blogPost = await BlogPost.create(req.body); 
-    res.json(blogPost);
+    const blog = await BlogPost.findByPk(id);
+    if (!blog) return res.status(400).json({ error: "Blog not found!" });
+    await blog.update(req.body);
+    res.json(blog);
   } catch (error) {
-    res.status(500).json({ error: error.message }); 
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteBlog = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const blog = await BlogPost.findByPk(id);
+    if (!blog) return res.status(400).json({ error: "Blog not found!" });
+    await blog.destroy();
+    res.json({ message: "Blog was deleted!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import "./db/index.js";
 import cors from "cors";
-import { createBlogPost } from "./controllers/blogPosts.js";
+import {
+  createBlogPost,
+  getBlogs,
+  getBlogById,
+  updateBlog,
+  deleteBlog,
+} from "./controllers/blogPosts.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,15 +15,13 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app
-  .route("/blogPosts")
-  .get((req, res) => res.json({ message: "GET ALL/" })).post(createBlogPost);
-  //.post((req, res) => res.json({ message: "POST /" }));
+app.route("/blogPosts").get(getBlogs).post(createBlogPost);
+//.get((req, res) => res.json({ message: "GET ALL/" }))
+//.post((req, res) => res.json({ message: "POST /" }));
 app
   .route("/blogPosts/:id")
-  .put((req, res) => res.json({ message: "PUT /" }))
-  .get((req, res) => res.json({ message: "GET /" }))
-  .delete((req, res) => res.json({ message: "DELETE /" }));
-
+  .put(updateBlog) //((req, res) => res.json({ message: "PUT /" }))
+  .get(getBlogById) //((req, res) => res.json({ message: "GET /" }))
+  .delete(deleteBlog); //((req, res) => res.json({ message: "DELETE /" }));
 
 app.listen(port, () => console.log(`Server is running on port ${port} `));
