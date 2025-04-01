f3d3bb74e1da9fdacdd8d5057d2d904f68fbc623
Hema Bindra <hbindra11@yahoo.com>
2025-04-01 13:22:43 +0200

    fixed bug preventing node index.js to run

--- db/index.js
+++ db/index.js
@@ -1,6 +1,12 @@
 // Import the Sequelize class from the library
 import { Sequelize } from 'sequelize';
+import dotenv from "dotenv";
+dotenv.config();
+
 // Create a new instance of Sequelize with the connection string
-const sequelize = new Sequelize(process.env.PG_URI);
+const sequelize = new Sequelize(process.env.PG_URI, {
+    //host: process.env.DB_HOST,
+    dialect: "postgres",
+  });
 // Export the instance so we can use it in other files
 export default sequelize; 
